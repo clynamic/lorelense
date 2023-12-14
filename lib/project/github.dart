efdea6b2d8869c13395fdbc854a82e5788763469
clragon <clragon@clynamic.net>
2023-12-14 23:02:49 +0100

    added right click homepage menu

--- lib/project/github.dart
+++ lib/project/github.dart
@@ -1,6 +1,8 @@
 import 'package:clynamic/client/models/github_project.dart';
 import 'package:clynamic/project/tile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_context_menu/flutter_context_menu.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:relative_time/relative_time.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
@@ -14,30 +16,62 @@ class GithubProjectTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: project.homepage != null
-          ? () => launchUrlString(project.homepage!)
-          : () => launchUrlString('https://github.com/${project.repo}'),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          ProjectBanner(url: project.banner),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  project.name,
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                const SizedBox(height: 8),
-                GithubProjectDescription(project: project),
-              ],
-            ),
+    String githubUrl = 'https://github.com/${project.owner}/${project.repo}';
+
+    void showMenu(Offset position) => showContextMenu<VoidCallback?>(
+          context,
+          transitionDuration: const Duration(milliseconds: 200),
+          reverseTransitionDuration: Duration.zero,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+              FadeTransition(
+            opacity: animation,
+            child: child,
           ),
-        ],
+          contextMenu: ContextMenu(
+            position: position,
+            entries: [
+              ListTileMenuItem(
+                leading: const FaIcon(FontAwesomeIcons.github),
+                title: const Text('Open GitHub page'),
+                onSelected: () => launchUrlString(githubUrl),
+              ),
+            ],
+          ),
+        );
+
+    return GestureDetector(
+      onLongPressStart: [
+        TargetPlatform.iOS,
+        TargetPlatform.android,
+      ].contains(Theme.of(context).platform)
+          ? (details) => showMenu(details.globalPosition)
+          : null,
+      onSecondaryTapDown: (details) => showMenu(details.globalPosition),
+      child: InkWell(
+        onTap: project.homepage != null
+            ? () => launchUrlString(project.homepage!)
+            : () => launchUrlString(githubUrl),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ProjectBanner(url: project.banner),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    project.name,
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  const SizedBox(height: 8),
+                  GithubProjectDescription(project: project),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -91,6 +125,45 @@ class GithubProjectDescription extends StatelessWidget {
           ],
         )
       ],
+    );
+  }
+}
+
+final class ListTileMenuItem<T> extends ContextMenuItem<T> {
+  const ListTileMenuItem({
+    super.value,
+    super.onSelected,
+    this.leading,
+    required this.title,
+  });
+
+  const ListTileMenuItem.submenu({
+    this.leading,
+    required this.title,
+    required List<ContextMenuEntry> super.items,
+    super.onSelected,
+  }) : super.submenu();
+
+  final Widget? leading;
+  final Widget title;
+
+  @override
+  Widget builder(
+    BuildContext context,
+    ContextMenuState menuState, [
+    FocusNode? focusNode,
+  ]) {
+    return Material(
+      type: MaterialType.transparency,
+      child: SizedBox(
+        width: 300,
+        child: ListTile(
+          leading: leading,
+          title: title,
+          trailing: isSubmenuItem ? const Icon(Icons.arrow_right) : null,
+          onTap: () => handleItemSelection(context),
+        ),
+      ),
     );
   }
 }
